ad9a6a7cde640f80b05acb20d3c129a6851fe84d
CelticSgc80 <sankhajitghoshchowdhury@gmail.com>
2024-03-23 12:59:33 +0530

    initial

---
+++ src/Auth/PrivateRoute.js
@@ -0,0 +1,24 @@
+import Cookies from "js-cookie";
+import CryptoJS from "crypto-js";
+
+
+
+function encrypt(text ) {
+  return CryptoJS.AES.encrypt(text, "poms-nic").toString();
+}
+
+
+function decrypt(ciphertext, key = "poms-nic") {
+  let bytes = CryptoJS.AES.decrypt(ciphertext, key);
+  return bytes.toString(CryptoJS.enc.Utf8);
+}
+
+const isLoggedIn = async () => {
+  let encryptUser;
+  while (!encryptUser) {
+      encryptUser = Cookies.get('_UR');
+      await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second before checking again
+  }
+  return encryptUser ? true : false;
+};
+export { encrypt,decrypt,isLoggedIn};
---
+++ src/Router/router.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from '../Pages/Home';
+import Error from '../Pages/Error';
+import UserLogin from '../Pages/Login';
+import Sidebarmenu from '../Pages/Menubar';
+import Reset from '../Pages/ResetPassword';
+import Profile from '../Pages/Profile';
+import Register from '../Pages/Registration';
+import Update from '../Pages/UserUpdate';
+
+import { isLoggedIn } from '../Auth/PrivateRoute';
+
+
+
+
+function MyRoute() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Register />} />
+            <Route path="/login" element={<UserLogin />} />
+            <Route path='/reset'  element={<Reset/>}/>
+            <Route path="/dashboard" element={isLoggedIn() ? <Sidebarmenu /> : <Navigate to="/error" />}>
+                <Route path='profile'  element={isLoggedIn() ? <Profile /> : <Navigate to="/error" />}/>
+                <Route path='update-user'  element={isLoggedIn() ? <Update /> : <Navigate to="/error" />}/>
+                <Route path="*" element={<Navigate to="/error" />} />
+            </Route>
+            <Route path="/error" element={<Error />} />
+            <Route path="*" element={<Error />} />
+        </Routes>
+    );
+}
+
+export default MyRoute;
