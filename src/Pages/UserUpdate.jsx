f44fea969bd9d8ee5d46dc8f88efb367b82e1b35
subham04mitra <subhammitra58@gmail.com>
2024-03-26 21:19:06 +0530

    update user

--- src/Pages/UserUpdate.jsx
+++ src/Pages/UserUpdate.jsx
@@ -21,8 +21,8 @@ function Update() {
     name: '',
     email: '',
     mobile: '',
-    college: '',
-    course: '',
+    company: '',
+    designation: '',
     city: '',
     pin_code: ''
   });
@@ -31,13 +31,23 @@ function Update() {
 
   useEffect(() => {
     const encryptUser = Cookies.get('_UR');
-    const encryptToken = Cookies.get('TK');
+    const encryptToken = Cookies.get('_TK');
 
     if (encryptUser && encryptToken) {
       const decryptedUser = decrypt(encryptUser);
       const decryptedToken = decrypt(encryptToken);
+      const userd=JSON.parse(decryptedUser)
       setUserData(JSON.parse(decryptedUser));
       setToken(JSON.parse(decryptedToken));
+      setUser({
+        name: userd.name || '',
+        email: userd.email || '',
+        mobile: userd.mobile || '',
+        company: userd.company || '',
+        designation: userd.designation || '',
+        city: userd.city || '',
+        pin_code: userd.pin_code || ''
+      });
     } else {
       // Handle case when cookies are not available
       // You may redirect user or show an error message
@@ -80,30 +90,32 @@ function Update() {
         position="top-center"
         reverseOrder={false}
       />
-      <MDBRow className='justify-content-center align-items-center m-5'>
+      <MDBRow className='justify-content-center align-items-center'>
         <MDBCard>
           <MDBCardBody className='px-4'>
             <h3 className="fw-bold mb-4 pb-2 pb-md-0 mb-md-5">Update User Details</h3>
             <MDBRow>
-              <MDBCol md='6'>
+              <MDBCol md='3'>
                 <MDBInput wrapperClass='mb-4' label='Name' size='lg' value={user.name} onChange={handleInput} name='name' type='text' />
               </MDBCol>
-              <MDBCol md='6'>
+              <MDBCol md='9'>
+                <MDBInput wrapperClass='mb-4' label='Company' size='lg' value={user.company} onChange={handleInput} name='college' type='text' />
+              </MDBCol>
+              
+            </MDBRow>
+            <MDBRow>
+            <MDBCol md='4'>
                 <MDBInput wrapperClass='mb-4' label='Email' size='lg' value={user.email} onChange={handleInput} name='email' type='email' />
               </MDBCol>
-            </MDBRow>
-            <MDBRow>
-              <MDBCol md='6'>
+              <MDBCol md='4'>
                 <MDBInput wrapperClass='mb-4' label='Mobile' size='lg' value={user.mobile} onChange={handleInput} name='mobile' type='number' />
               </MDBCol>
-              <MDBCol md='6'>
-                <MDBInput wrapperClass='mb-4' label='Course' size='lg' value={user.course} onChange={handleInput} name='course' type='text' />
+              <MDBCol md='4'>
+                <MDBInput wrapperClass='mb-4' label='Designation' size='lg' value={user.designation} onChange={handleInput} name='course' type='text' />
               </MDBCol>
             </MDBRow>
             <MDBRow>
-              <MDBCol md='12'>
-                <MDBInput wrapperClass='mb-4' label='College' size='lg' value={user.college} onChange={handleInput} name='college' type='text' />
-              </MDBCol>
+              
             </MDBRow>
             <MDBRow>
               <MDBCol md='6'>
