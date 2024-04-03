31d2b40a309ecc684ca34bfa31c0196858358784
technoid2023 <136116887+technoid2023@users.noreply.github.com>
2024-04-03 17:20:50 +0530

    Update Menubar.jsx

--- src/Pages/Menubar.jsx
+++ src/Pages/Menubar.jsx
@@ -1,305 +1,105 @@
+import React, { useState } from 'react';
+import { Sidebar, MenuItem ,Menu} from 'react-pro-sidebar';
+import Layout from '../Components/Layout/Layout';
 
-import React, { useEffect, useState } from 'react';
-import toast from "react-hot-toast";
+
+import './SidebarMenu.css';
+import {faUserTie,faBell,faBoxOpen,faStore ,faSignOut,faBars,faWarehouse,faCashRegister} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { Outlet, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 import Cookies from 'js-cookie';
-import { decrypt } from '../Auth/PrivateRoute';
-import axios from 'axios';
-import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBTable, MDBTableBody, MDBTableHead, MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBCheckbox } from 'mdb-react-ui-kit';
-import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
-const BillForm = () => {
+
+const Sidebarmenu = () => {
+    const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
+
   const navigate=useNavigate();
-  const [itemList, setItemList] = useState();
-  const [selectedItem, setSelectedItem] = useState();
-  const [quantity, setQuantity] = useState();
-  const [chkQty, setChkQty] = useState();
-  const [itemRate, setItemRate] = useState();
-  const [token, setToken] = useState(null);
-  const [cusName, setCusName] = useState();
-  const [cusEmail, setCusEmail] = useState();
-  const [cusMobile, setCusMobile] = useState();
-  const [cusAddress, setCusAddress] = useState();
-  const [isPaid, setIsPaid] = useState(false);
-  const [billItems, setBillItems] = useState([]);
-  const [filteredItems, setFilteredItems] = useState([]);
 
-  useEffect(() => {
-    const encryptToken = Cookies.get('_TK');
-
-    if ( encryptToken) {
-      const decryptedToken = decrypt(encryptToken);
-    
-      setToken(JSON.parse(decryptedToken));
-    } 
-  }, []);
-
-  const handleAddItem = () => {
-    if (selectedItem && quantity && itemRate) {
-      if (parseInt(quantity) > parseInt(chkQty)) {
-        toast.error(`Stock not available \n Available Stock ${chkQty}`);
-        return;
-      }
-      const newItem = {
-        item_cd: selectedItem,
-        item_qty: quantity,
-        item_rate: itemRate,
-        amount:(itemRate*quantity)
-      };
-      setBillItems([...billItems, newItem]);
-      setQuantity('');
-      setSelectedItem('');
-      setItemRate('');
-    }
-  };
-
-  const handleDeleteItem = (index) => {
-    const updatedItems = [...billItems];
-    updatedItems.splice(index, 1);
-    setBillItems(updatedItems);
-  };
-
-  const handleGenerateBill = () => {
-    const formData = {
-      cus_name: cusName,
-      cus_email: cusEmail,
-      cus_mobile: cusMobile,
-      cus_address: cusAddress,
-      items: billItems,
-      pay: isPaid 
-    };
-    if (formData.cus_name && formData.cus_email && formData.cus_mobile && formData.cus_address && formData.items) {
-      axios.post('https://edu-tech-bwe5.onrender.com/v1/bill', formData, {
-        headers: {
-          'token': token
-        }
-      })
-      .then(response => {
-        console.log(response);
-        if (response.data.Success === true) {
-          toast.success(`Bill generated, Bill No:${response.data.Bill_no}`);
-
-          clearForm();
-          navigate('/dashboard/bill')
-        } else {
-          toast.error('Failed to generate bill.');
-        }
-      })
-      .catch(error => {
-        console.error('An error occurred while generating the bill:', error);
-      });
-    } else {
-      toast.error("Fill the form")
-    }
-  };
-
-  const clearForm = () => {
-    setSelectedItem('');
-    setQuantity('');
-    setItemRate('');
-    setCusName('');
-    setCusEmail('');
-    setCusMobile('');
-    setCusAddress('');
-    setIsPaid(false);
-    setBillItems([]);
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://edu-tech-bwe5.onrender.com/v1/item', {
-          headers: {
-            'token': token
-          }
-        });
-        if (response.data.Success === true) {
-          setItemList(response.data.Data);
-          setFilteredItems(response.data.Data);
-        } else if (response.data.Data === undefined) {
-          setItemList(['Loading']);
-        } else {
-          console.log('Data not found');
-        }
-      } catch (error) {
-        console.error('An error occurred:', error);
-      }
+    const toggleSidebar = () => {
+        setSidebarCollapsed(!sidebarCollapsed);
     };
 
-    fetchData();
-  }, [token]);
-
-  const handleSearch = (searchTerm) => {
-    if (!searchTerm) {
-      setFilteredItems(itemList);
-      return;
+const handleLogout = () => {
+    const confirmLogout = window.confirm('Are you sure you want to log out?');
+    if (confirmLogout) {
+        Cookies.remove('_UR');
+        toast.success('Logged Out !');
+        Cookies.remove('_TK')
+        setTimeout(() => {
+            navigate('/login');
+        }, 500);
     }
-
-    const filtered = itemList.filter(
-      (item) =>
-        item.item_cd.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        item.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setFilteredItems(filtered);
-  };
-
-  const handleItemSelected = (item) => {
-    setSelectedItem(item.item_cd);
-    setItemRate(item.SP);
-    setChkQty(item.qty);
-  };
-
-  return (
-    <MDBContainer style={{ backgroundColor: 'white', padding: '20px', borderRadius: '10px', boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.1)', overflowX: 'auto' }}>
-      <MDBRow>
-        <MDBCol>
-          <h2 style={{textAlign:'center', fontFamily:'sans-serif', color:'blue', fontWeight:'bolder'}}>Bill Entry Form</h2>
-        </MDBCol>
-      </MDBRow>
-
-      {/* Customer Section */}
-      <MDBRow className="mb-3">
-        <MDBCol md='3'>
-          <MDBInput
-            type="text"
-            label="Name(Customer)"
-            value={cusName}
-            onChange={(e) => setCusName(e.target.value)}
-            style={{ width: '100%' }}
-          /><br></br>
-        </MDBCol>
-     
-        <MDBCol md='3'>
-          <MDBInput
-            type="email"
-            label="Email(Customer)"
-            value={cusEmail}
-            onChange={(e) => setCusEmail(e.target.value)}
-            style={{ width: '100%' }}
-          /><br></br>
-        </MDBCol>
-       
-        <MDBCol md='2'>
-          <MDBInput
-            type="tel"
-            label="Mobile(Customer)"
-            value={cusMobile}
-            onChange={(e) => setCusMobile(e.target.value)}
-            style={{ width: '100%' }}
-          /><br></br>
-        </MDBCol>
-       
-        <MDBCol md='4'>
-          <MDBInput
-            type="text"
-            label="Address(Customer)"
-            value={cusAddress}
-            onChange={(e) => setCusAddress(e.target.value)}
-            style={{ width: '100%' }}
-          /><br></br>
-        </MDBCol>
-        
-      </MDBRow>
-
-      {/* Item Section */}
-      <MDBRow className="mb-2">
-        {/* Item Dropdown and Input Fields */}
-        <MDBCol md='2'>
-          <MDBDropdown style={{ width: '6rem' }}>
-            <MDBDropdownToggle style={{ width: '8rem' }}>
-              {selectedItem ? selectedItem : 'Select Item'}
-            </MDBDropdownToggle >
-            <MDBDropdownMenu>
-              <MDBInput type="text" label="Search" onChange={(e) => handleSearch(e.target.value)} />
-              {filteredItems.map((item, index) => (
-                <MDBDropdownItem key={index} onClick={() => handleItemSelected(item)}>
-                  {item.item_cd} - {item.name}
-                </MDBDropdownItem>
-              ))}
-            </MDBDropdownMenu>
-          </MDBDropdown><br></br>
-        </MDBCol>
-        {/* Other Input Fields */}
-        <MDBCol md='2'>
-          <MDBInput
-            type="number"
-            label="Quantity"
-            value={quantity}
-            onChange={(e) => setQuantity(e.target.value)}
-            style={{ width: '100%' }}
-          /><br></br>
-        </MDBCol>
-       
-        <MDBCol md='2'>
-          
-          <MDBInput
-            type="number"
-            label="Item Rate"
-            value={itemRate}
-            onChange={(e) => setItemRate(e.target.value)}
-            style={{ width: '100%' }}
-          /><br></br>
-          
-        </MDBCol>
-        <MDBCol>
-          <MDBBtn onClick={handleAddItem}>Add</MDBBtn>
-        </MDBCol>
-      </MDBRow>
-
-      {/* Table Section */}
-      <MDBRow>
-        <MDBCol>
-          <MDBTable>
-            <MDBTableHead style={{ backgroundColor: '#f8f9fa' }}>
-              <tr>
-                <th>Item</th>
-                <th>Quantity</th>
-                <th>Item Rate</th>
-                <th>Amount</th>
-                <th>Action</th>
-              </tr>
-            </MDBTableHead>
-            <MDBTableBody>
-              {billItems.map((i, index) => (
-                <tr key={index}>
-                  <td>{i.item_cd}</td>
-                  <td>{i.item_qty}</td>
-                  <td>{i.item_rate}</td>
-                  <td>{i.amount}</td>
-                  <td>
-                    <MDBBtn onClick={() => handleDeleteItem(index)}>Delete</MDBBtn>
-                  </td>
-                </tr>
-              ))}
-            </MDBTableBody>
-          </MDBTable>
-        </MDBCol>
-      </MDBRow>
-
-      {/* Checkbox Section */}
-      <MDBRow className="mb-3">
-        <MDBCol>
-          <MDBCheckbox
-            id="isPaid"
-            label="Is Paid"
-            checked={isPaid}
-            onChange={() => setIsPaid(!isPaid)}
-          />
-        </MDBCol>
-      </MDBRow>
-
-      <MDBRow className="justify-content-center">
-  <MDBCol md="6">
-    <div className="text-center">
-      <MDBBtn onClick={handleGenerateBill} style={{marginRight:'5px'}}>Generate Bill</MDBBtn>
-      <MDBBtn onClick={clearForm} >Clear</MDBBtn>
-    </div>
-  </MDBCol>
-</MDBRow>
-
-
-
-    </MDBContainer>
-  );
 };
 
-export default BillForm;
+
+const menuItems = [
+    { name: 'Profile', icon: faUserTie, link: 'profile' },
+    { name: 'Inventory', icon: faWarehouse, link: 'item' },
+    { name: 'Billing', icon: faCashRegister, link: 'bill' },
+    { name: 'Orders', icon: faStore, link: '/marketing' },
+    { name: 'Stocks', icon: faBoxOpen, link: 'stock' },
+    { name: 'Notifiation', icon: faBell, link: '/production-development' },
+];
+
+    
+        return (
+            <Layout>
+                <div className="sidebar-menu-container">
+                  
+                    <div className={`sidebar-container ${sidebarCollapsed ? 'collapsed' : ''}`}  >
+                        <Sidebar className="sidebar" collapsed={sidebarCollapsed}
+                     
+                        >
+                        <Menu>
+                        {menuItems.map((item, index) => (
+                            <Link to={item.link}>
+                                <MenuItem key={index} icon={<FontAwesomeIcon icon={item.icon} size='1x' style={{ color: 'saddlebrown', marginRight: '10px' }} />}>
+                                
+                                    {item.name}
+                                
+                            </MenuItem>
+                            </Link>
+                        ))}
+                    </Menu>
+
+                           
+                            <div style={{display:'flex',alignItems:'center'}} onClick={handleLogout}>
+                                <Link style={{marginLeft:'9rem',marginTop:'1rem'}}>
+                                    <FontAwesomeIcon icon={faSignOut} size='1x' style={{ color: 'red', marginRight: '10px' }}/>
+                                </Link>
+                                <Link style={{marginTop:'1rem',color:'red'}}>Log Out</Link>
+                            </div>
+                        </Sidebar>
+                    </div>
+                    <div
+                        className="sidebar-toggler"
+                        onClick={toggleSidebar}
+                        style={{
+                            top: sidebarCollapsed ? '1rem' : '1rem',
+                            left: sidebarCollapsed ? '1rem' : '1rem',
+                        }}
+                    >
+                    <Link> <FontAwesomeIcon icon={faBars} size='1x' style={{ color: 'white' }} /> </Link>
+                    </div>
+                    <main className="main" style={{
+                        backgroundColor: 'white',
+                        // backgroundImage: `url(${background})`,
+                        borderRadius: '10px',
+                        boxShadow: 'rgba(0, 0, 0, 0.6) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(0, 0, 0, 0.4) 0px -2px 6px 0px inset',
+                        padding: '1px',
+                        margin: '1px',
+                        maxHeight:'45rem'
+                    }}><Outlet />
+                    </main>
+                </div>
+            </Layout>
+        );
+    
+};
+
+
+export default Sidebarmenu;
